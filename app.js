db67ec008d620f507464e182b68dde50a5de1ed6
Ricardo <descoubet0107@gmail.com>
2024-04-06 16:03:35 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const { crearArchivo } = require('./helpers/multiplicar.js');
+const argv = require('./config/yargs.js');
+require('colors');
+
+console.clear();
+
+crearArchivo(argv.b, argv.l, argv.h)
+	.then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+	.catch(err => console.log(err));
